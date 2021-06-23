863d8bc5cde0c71871898f7dc9d20a4639fc49ea
Leo Preciado <leoipn@gmail.com>
2021-06-22 20:07:15 -0500

    Javascript challenge - Final

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -14,3 +14,39 @@ tableData.forEach((ufoSights) => {
     });
   });
   
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+  
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+  
+    // Get the value property of the input element
+    var value = inputElement.property("value");
+  
+    // Use the form input to filter the data by blood type
+    var filteredDateTime = tableData.filter(element => element.datetime == value);
+    console.log(filteredDateTime);
+    
+    tbody.html("");
+    
+    filteredDateTime.forEach(element => {
+        var row = tbody.append("tr");
+        Object.entries(element).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+    });
+}
